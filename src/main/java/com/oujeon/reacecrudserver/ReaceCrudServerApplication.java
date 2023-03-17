70b53c42df2d53005d4492f60b517ba2f255e9cd
oujeon <oujeon@gmail.com>
2023-03-17 13:28:10 +0900

    최초등록

---
+++ src/main/java/com/oujeon/reacecrudserver/ReaceCrudServerApplication.java
@@ -0,0 +1,13 @@
+package com.oujeon.reacecrudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaceCrudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaceCrudServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oujeon/reacecrudserver/ReaceCrudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oujeon.reacecrudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaceCrudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
